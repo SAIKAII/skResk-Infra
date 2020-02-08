c63c3b5a26b32ba2b49cb3ed445a3ae9d5314785
SAIKAII <990394572@qq.com>
2020-02-08 23:35:47 +0800

    添加用于记录应用程序对应使用的负载均衡算法的变量与处理，算法用于从众多应用实例中选取一个以提供服务

--- lb/app.go
+++ lb/app.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 )
 
+var lbs = make(map[string]Balancer, 0)
+
 type Apps struct {
 	Client *eureka.Client
 }
@@ -16,28 +18,39 @@ type App struct {
 	lb        Balancer
 }
 
+// AddBalancer 添加应用对应的负载均衡算法，或替换已有应用的负载均衡算法
+func AddBalancer(appName string, lb Balancer) {
+	lbs[appName] = lb
+}
+
 func (a *App) Get(key string) *ServerInstance {
 	return a.lb.Next(key, a.Instances)
 }
 
 func (a *Apps) Get(appName string) *App {
-	var app *eureka.Application
+	var application *eureka.Application
 	for _, a := range a.Client.Applications.Applications {
 		if a.Name == strings.ToUpper(appName) {
-			app = &a
+			application = &a
 			break
 		}
 	}
-	if app == nil {
+	if application == nil {
 		return nil
 	}
 
-	na := &App{
-		Name:      app.Name,
-		Instances: make([]*ServerInstance, 0),
-		lb:        &RoundRobinBalancer{},
+	lb, ok := lbs[appName]
+	if !ok {
+		lb = &RoundRobinBalancer{}
+		lbs[appName] = lb
 	}
-	for _, ins := range app.Instances {
+
+	na := &App{
+		Name:      application.Name,
+		Instances: make([]*ServerInstance, 0),
+		lb:        lb,
+	}
+	for _, ins := range application.Instances {
 		var port int
 		if ins.SecurePort.Enabled {
 			port = ins.SecurePort.Port
